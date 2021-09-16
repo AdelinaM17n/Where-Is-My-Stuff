8af022fe105a4f010945c2a8a5634e55bfa4f7fb
Maheesha <maheeshasithum@gmail.com>
2021-09-16 19:35:46 +0530

    some fine tuining

--- src/main/java/io/github/maheevil/mixin/DeathScreenMixin.java
+++ src/main/java/io/github/maheevil/mixin/DeathScreenMixin.java
@@ -1,16 +1,23 @@
 package io.github.maheevil.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.DeathScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(DeathScreen.class)
 public class DeathScreenMixin extends Screen {
+    @Shadow @Final private List<Button> exitButtons;
+
     protected DeathScreenMixin(Component component) {
         super(component);
     }
@@ -28,9 +35,10 @@ public class DeathScreenMixin extends Screen {
     public void render(PoseStack poseStack, int i, int j, float f, CallbackInfo ci){
         assert this.minecraft != null;
         assert this.minecraft.player != null;
+        System.out.println(this.height);
         String xyz = "[X: " + minecraft.player.blockPosition().getX() + "/ Y: " + minecraft.player.blockPosition().getY() + "/ Z: " + minecraft.player.blockPosition().getZ() + "]";
         String zyx = "XYZ: " + this.minecraft.player.blockPosition().getX() + " / " + minecraft.player.blockPosition().getY() + " / " + minecraft.player.blockPosition().getZ();
-        drawCenteredString(poseStack,this.font, xyz, this.width / 2, 115, 16777215);
+        drawCenteredString(poseStack,this.font, xyz, this.width / 2, this.height - this.height / 5, 16777215);
 
     }
 }
